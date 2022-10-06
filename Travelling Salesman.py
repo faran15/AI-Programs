240f7234cea6980a2c551bbe7d60de8b5cb5942c
faran15 <113420786+faran15@users.noreply.github.com>
2022-10-06 13:40:29 +0530

    Update Travelling Salesman.py

--- Travelling Salesman.py
+++ Travelling Salesman.py
@@ -35,4 +35,8 @@ if __name__ == "__main__":
     graph = [[0, 10, 15, 20], [10, 0, 35, 25],
             [15, 35, 0, 30], [20, 25, 30, 0]]
     s = 0
-    print(travellingSalesmanProblem(graph, s))        
+    print(travellingSalesmanProblem(graph, s)) 
+  
+  Sample Output :
+
+  80   
